68c814e63347801b185147243793eb0c34d8639e
Jonathan Junge <jonathan.junge96@gmail.com>
2026-01-28 15:57:45 +0100

    Initial Comint

---
+++ GameRating_Api_App/Data/GameData.cs
@@ -0,0 +1,47 @@
+﻿using GameRating_Api_App.Models;
+
+namespace GameRating_Api_App.Data
+{
+    public static class GameData
+    {
+        public static List<Game> Ratings { get; set; } = new()
+        {
+            new Game
+            {
+                Id = 1,
+                Title = "The Witcher 3",
+                Developer = "CD Projekt Red",
+                Year = 2015,
+                Rating = 5,
+                Review = "Ett fantastiskt rollspel med djup story."
+            },
+            new Game
+            {
+                Id = 2,
+                Title = "Elden Ring",
+                Developer = "FromSoftware",
+                Year = 2022,
+                Rating = 5,
+                Review = "Kreativt och tidlöst spel."
+            },
+            new Game
+            {
+                Id= 3,
+                Title = "Apex Legends",
+                Developer = "Electronic Arts",
+                Year = 2019,
+                Rating = 3,
+                Review = "Bra koncept"
+            },
+            new Game
+            {
+                Id= 4,
+                Title = "Battlefield 6",
+                Developer = "Battlefield Studios",
+                Year = 2025,
+                Rating = 4,
+                Review = "Kul i början, men långtråkigt."
+            }
+        };
+    }
+}
---
+++ GameRating_Api_App/Services/GameService.cs
@@ -0,0 +1,50 @@
+﻿using GameRating_Api_App.Data;
+using GameRating_Api_App.Models;
+
+namespace GameRating_Api_App.Services
+{
+    public class GameService
+    {
+        public List<Game> GetAll()
+        {
+            return GameData.Ratings;
+        }
+
+        public Game GetById(int id)
+        {
+            return GameData.Ratings.FirstOrDefault(g => g.Id == id);
+        }
+
+        public Game Add(Game rating)
+        {
+            rating.Id = GameData.Ratings.Max(g => g.Id) + 1;
+            GameData.Ratings.Add(rating);
+            return rating;
+        }
+
+        public Game Update(int id, Game updated)
+        {
+            var existing = GetById(id);
+            if (existing == null)
+                return null;
+
+            existing.Title = updated.Title;
+            existing.Developer = updated.Developer;
+            existing.Year = updated.Year;
+            existing.Rating = updated.Rating;
+            existing.Review = updated.Review;
+
+            return existing;
+        }
+
+        public bool Delete(int id)
+        {
+            var rating = GetById(id);
+            if (rating == null)
+                return false;
+
+            GameData.Ratings.Remove(rating);
+            return true;
+        }
+    }
+}
